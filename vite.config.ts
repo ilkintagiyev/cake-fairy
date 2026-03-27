c79cc4cda54bc97be7c26fa676e70768b7ab4752
ilkintagiyev <taghiyevilkin41@gmail.com>
2026-03-27 17:00:06 +0400

    versel added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Layihənin kök qovluqdan başladığını dəqiqləşdiririk
+  build: {
+    outDir: 'dist', // Vercel-in hara baxacağını bildiyindən əmin oluruq
+  }
 })
